149adcbf993db6ee3eb7a2899780eceb05e1a804
Enda Kelly <kelly.enda@gmail.com>
2023-01-03 13:37:10 +0000

    added: websockets for driver pool + joining driver pool

--- server/trips/consumers.py
+++ server/trips/consumers.py
@@ -1,5 +1,5 @@
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
-
+from channels.db import database_sync_to_async
 
 
 class TaxiConsumer(AsyncJsonWebsocketConsumer):
@@ -10,24 +10,36 @@ class TaxiConsumer(AsyncJsonWebsocketConsumer):
         if user.is_anonymous:
             await self.close()
         else:
-            await self.channel_layer.group_add(
-                group='test',
-                channel=self.channel_name
-            )
+            user_group = await self._get_user_group(user)
+            if user_group == 'driver':
+                await self.channel_layer.group_add(
+                    group='drivers',
+                    channel=self.channel_name
+                )
             await self.accept()
 
+
     async def disconnect(self, code):
-        await self.channel_layer.group_discard(
-            group='test',
-            channel=self.channel_name
-        )
+        user = self.scope['user']
+
+        if user.is_anonymous:
+            await self.close()
+        else:
+            user_group = await self._get_user_group(user)
+            if user_group == 'driver':
+                await self.channel_layer.group_discard(
+                    group='drivers',
+                    channel=self.channel_name
+                )
         await super().disconnect(code)
+
 
     async def echo_message(self, message):
         await self.send_json({
             'type': message.get('type'),
             'data': message.get('data'),
         })
+
 
     async def receive_json(self, content, **kwargs):
         message_type = content.get('type')
@@ -36,3 +48,7 @@ class TaxiConsumer(AsyncJsonWebsocketConsumer):
                 'type': message_type,
                 'data': content.get('data'),
             })
+            
+    @database_sync_to_async
+    def _get_user_group(self, user):
+        return user.groups.first().name
