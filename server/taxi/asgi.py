8306f411f782ec187c2c00936e063406c0578ab0
Enda Kelly <kelly.enda@gmail.com>
2023-01-03 13:27:51 +0000

    added: websockets incl. auth via socket connection

--- server/taxi/asgi.py
+++ server/taxi/asgi.py
@@ -11,15 +11,18 @@ import os
 
 from django.core.asgi import get_asgi_application
 from django.urls import path
-
 from channels.routing import ProtocolTypeRouter, URLRouter
+
+from taxi.middleware import TokenAuthMiddlewareStack
 from trips.consumers import TaxiConsumer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taxi.settings')
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
-    'websocket': URLRouter([
-        path('taxi/', TaxiConsumer.as_asgi()),
-    ]),
+    'websocket': TokenAuthMiddlewareStack(
+        URLRouter([
+            path('taxi/', TaxiConsumer.as_asgi()),
+        ])
+    ),
 })
