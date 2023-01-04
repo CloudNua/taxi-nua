0aacc95979acde444dbf4fb11bc48d90269df9cc
Enda Kelly <kelly.enda@gmail.com>
2023-01-04 21:29:52 +0000

    added: initial logic for user photos

--- server/taxi/urls.py
+++ server/taxi/urls.py
@@ -13,8 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework_simplejwt.views import TokenRefreshView
 
 from trips.views import SignUpView, LogInView
@@ -25,4 +28,4 @@ urlpatterns = [
     path('api/log_in/', LogInView.as_view(), name='log_in'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/trip/', include('trips.urls', 'trip',)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
