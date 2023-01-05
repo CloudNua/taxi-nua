489b7b5a2f30bc5b4ea34dc1ab2e081880e6eeca
Enda Kelly <kelly.enda@gmail.com>
2023-01-05 19:10:25 +0000

    added: nav test 2

--- client/cypress/e2e/navigation.cy.js
+++ client/cypress/e2e/navigation.cy.js
@@ -4,7 +4,6 @@ describe('Navigation', function () {
     cy.get('a').contains('Sign up').click();
     cy.hash().should('eq', '#/sign-up');
   });
-});
 
   // new
   it('Can navigate to log in from home', function () {
@@ -12,3 +11,11 @@ describe('Navigation', function () {
     cy.get('a').contains('Log in').click();
     cy.hash().should('eq', '#/log-in');
   });
+
+  // new
+  it('Can navigate to home from sign up', function () {
+    cy.visit('/#/sign-up');
+    cy.get('a').contains('Home').click();
+    cy.hash().should('eq', '#/');
+  });
+});
