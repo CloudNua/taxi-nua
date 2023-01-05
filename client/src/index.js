93ad6b12ab4287ad21611c0be249dda77fd01600
Enda Kelly <kelly.enda@gmail.com>
2023-01-05 18:40:17 +0000

    added: new react routing

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter, Route, Routes } from 'react-router-dom'; // new
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
 import App from './App';
+// new begin
+import Landing from './components/Landing';
+import LogIn from './components/LogIn';
+import SignUp from './components/SignUp';
+// new end
 import reportWebVitals from './reportWebVitals';
 
-// changed
 ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
       <Routes>
         <Route path='/' element={<App />} />
+        {/* new begin */}
+        <Route index element={<Landing />} />
+        <Route path='sign-up' element={<SignUp />} />
+        <Route path='log-in' element={<LogIn />} />
+        {/* new end */}
       </Routes>
     </HashRouter>
   </React.StrictMode>,
