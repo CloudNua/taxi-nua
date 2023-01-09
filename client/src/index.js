eac59dd37754cd0a797034e550d9ad9f66ba02cc
Enda Kelly <kelly.enda@gmail.com>
2023-01-09 22:23:21 +0000

    added: bootstrap css + dom bindings

--- client/src/index.js
+++ client/src/index.js
@@ -1,25 +1,24 @@
+import 'bootswatch/dist/lumen/bootstrap.css'; // new
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
 import App from './App';
-// new begin
 import Landing from './components/Landing';
 import LogIn from './components/LogIn';
 import SignUp from './components/SignUp';
-// new end
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
       <Routes>
-        <Route path='/' element={<App />} />
-        {/* new begin */}
-        <Route index element={<Landing />} />
-        <Route path='sign-up' element={<SignUp />} />
-        <Route path='log-in' element={<LogIn />} />
-        {/* new end */}
+        <Route path='/' element={<App />}>
+          <Route index element={<Landing />} />
+          <Route path='sign-up' element={<SignUp />} />
+          <Route path='log-in' element={<LogIn />} />
+        </Route>
       </Routes>
     </HashRouter>
   </React.StrictMode>,
