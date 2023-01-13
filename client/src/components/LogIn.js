bf4365029c339ac664b4a09f690bf680c307ac7a
Enda Kelly <38502722+endk17@users.noreply.github.com>
2023-01-13 18:46:04 +0000

    feat: react forms (#3)
    * added: react forms using formik for login
    
    * added: functional formik section
    
    * added: js func for page redirect on login
    
    * added: cypress test for auth and formik for signup
    
    * added: redirect on sign up

--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -1,19 +1,38 @@
+import React, { useState } from 'react'; // changed
+import { Formik } from 'formik';
+import {
+  Breadcrumb, Button, Card, Form
+} from 'react-bootstrap';
+import { Link, Navigate } from 'react-router-dom'; // changed
 
-import React from 'react';
-import { Breadcrumb, Card } from 'react-bootstrap'; // new
-import { Link } from 'react-router-dom';
-
-// changed
 function LogIn (props) {
+
+  const [isSubmitted, setSubmitted] = useState(false);
+  const onSubmit = (values, actions) => setSubmitted(true);
+
+  if (isSubmitted) {
+    return <Navigate to='/' />;
+  }
+
+
   return (
     <>
       <Breadcrumb>
-        <Breadcrumb.Item href='/#/'>Home</Breadcrumb.Item>
+        <Breadcrumb.Item href='/'>Home</Breadcrumb.Item>
         <Breadcrumb.Item active>Log in</Breadcrumb.Item>
       </Breadcrumb>
       <Card>
         <Card.Header>Log in</Card.Header>
         <Card.Body>
+          <Formik
+            initialValues={{
+              username: '',
+              password: ''
+            }}
+            onSubmit={onSubmit} {/* changed */}
+          >
+            {/* hidden for clarity */}
+          </Formik>
           <Card.Text className='text-center'>
             Don't have an account? <Link to='/sign-up'>Sign up!</Link>
           </Card.Text>
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,18 +1,42 @@
-import React from 'react';
-import { Breadcrumb, Card } from 'react-bootstrap'; // new
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react'; // changed
+import { Formik } from 'formik';
+import {
+  Breadcrumb, Button, Card, Form
+} from 'react-bootstrap';
+import { Link, Navigate } from 'react-router-dom'; // changed
 
-// changed
 function SignUp (props) {
+  // new begin
+  const [isSubmitted, setSubmitted] = useState(false);
+  const onSubmit = (values, actions) => setSubmitted(true);
+
+  if (isSubmitted) {
+    return <Navigate to='/log-in' />;
+  }
+  // new end
+
   return (
     <>
       <Breadcrumb>
-        <Breadcrumb.Item href='/#/'>Home</Breadcrumb.Item>
+        <Breadcrumb.Item href='/'>Home</Breadcrumb.Item>
         <Breadcrumb.Item active>Sign up</Breadcrumb.Item>
       </Breadcrumb>
-      <Card>
+      <Card className='mb-3'>
         <Card.Header>Sign up</Card.Header>
         <Card.Body>
+          <Formik
+            initialValues={{
+              username: '',
+              firstName: '',
+              lastName: '',
+              password: '',
+              group: 'rider',
+              photo: []
+            }}
+            onSubmit={onSubmit} {/* changed */}
+          >
+            {/* hidden for clarity */}
+          </Formik>
           <Card.Text className='text-center'>
             Already have an account? <Link to='/log-in'>Log in!</Link>
           </Card.Text>
