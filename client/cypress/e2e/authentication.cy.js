130913a98ca9aec4bde4079f1b200d007112c041
Enda Kelly <38502722+endk17@users.noreply.github.com>
2023-01-21 13:53:58 +0000

    feat: http login logout (#4)
    * added: log in app
    
    * added: root element to layout
    
    * added: new element to app
    1. hook for login
    2. requirement for login() method
    3. nav bar for log out btn
    4. logIn() method for LogIn component
    
    * added: Axios for api to server call path
    
    * added: isLoggedIn() checks window.localStorage for JWT object
    
    * added: new path for logIn, signUp + Landing
    
    * added: refactor tests
    
    * added: server error mngt + log out func

--- client/cypress/e2e/authentication.cy.js
+++ client/cypress/e2e/authentication.cy.js
@@ -1,14 +1,33 @@
 
+
+const logIn = () => {
+  const { username, password } = Cypress.env('credentials');
+
+  // Capture HTTP requests.
+  cy.intercept('POST', 'log_in', {
+    statusCode: 200,
+    body: {
+      'access': 'ACCESS_TOKEN',
+      'refresh': 'REFRESH_TOKEN'
+    }
+  }).as('logIn');
+
+  // Log into the app.
+  cy.visit('/#/log-in');
+  cy.get('input#username').type(username);
+  cy.get('input#password').type(password, { log: false });
+  cy.get('button').contains('Log in').click();
+  cy.wait('@logIn');
+};
+
+
 describe('Authentication', function () {
     it('Can log in.', function () {
-      cy.visit('/#/log-in');
-      cy.get('input#username').type('gary.cole@example.com');
-      cy.get('input#password').type('pAssw0rd', { log: false });
-      cy.get('button').contains('Log in').click();
+      logIn();
       cy.hash().should('eq', '#/');
+      cy.get('button').contains('Log out');
     });
-  
-    // new
+
     it('Can sign up.', function () {
       cy.visit('/#/sign-up');
       cy.get('input#username').type('gary.cole@example.com');
@@ -20,4 +39,55 @@ describe('Authentication', function () {
       cy.get('button').contains('Sign up').click();
       cy.hash().should('eq', '#/log-in');
     });
-  });
+
+    it('Can log out.', function () {
+      logIn();
+      cy.get('[data-cy="logOut"]').click().should(() => {
+        expect(window.localStorage.getItem('taxi.auth')).to.be.null;
+      });
+      cy.get('[data-cy="logOut"]').should('not.exist');
+    });
+    
+
+    it('Cannot visit the login page when logged in.', function () {
+      logIn();
+      cy.visit('/#/log-in');
+      cy.hash().should('eq', '#/');
+    });
+
+    it('Cannot visit the sign up page when logged in.', function () {
+      logIn();
+      cy.visit('/#/sign-up');
+      cy.hash().should('eq', '#/');
+    });
+
+    it('Cannot see links when logged in.', function () {
+      logIn();
+      cy.get('[data-cy="signUp"]').should('not.exist');
+      cy.get('[data-cy="logIn"]').should('not.exist');
+    });
+
+    it('Shows an alert on login error.', function () {
+      const { username, password } = Cypress.env('credentials');
+      cy.intercept('POST', 'log_in', {
+        statusCode: 400,
+        body: {
+          __all__: [
+            'Please enter a correct username and password. ' +
+            'Note that both fields may be case-sensitive.'
+          ]
+        }
+      }).as('logIn');
+      cy.visit('/#/log-in');
+      cy.get('input#username').type(username);
+      cy.get('input#password').type(password, { log: false });
+      cy.get('button').contains('Log in').click();
+      cy.wait('@logIn');
+      cy.get('div.alert').contains(
+        'Please enter a correct username and password. ' +
+        'Note that both fields may be case-sensitive.'
+      );
+      cy.hash().should('eq', '#/log-in');
+    });
+  
+});
