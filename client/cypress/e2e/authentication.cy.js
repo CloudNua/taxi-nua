a85fe2650dd426019b2abba13c58f666198a03be
Enda Kelly <kelly.enda@gmail.com>
2023-04-05 22:54:22 +0100

    feat: https requests for sign up

--- client/cypress/e2e/authentication.cy.js
+++ client/cypress/e2e/authentication.cy.js
@@ -29,23 +29,32 @@ describe('Authentication', function () {
     });
 
     it('Can sign up.', function () {
+      // new
+      cy.intercept('POST', 'sign_up', {
+        statusCode: 201,
+        body: {
+          id: 1,
+          username: 'gary.cole@example.com',
+          first_name: 'Gary',
+          last_name: 'Cole',
+          group: 'driver',
+          photo: '/media/images/photo.jpg'
+        }
+      }).as('signUp');
+
       cy.visit('/#/sign-up');
       cy.get('input#username').type('gary.cole@example.com');
       cy.get('input#firstName').type('Gary');
       cy.get('input#lastName').type('Cole');
       cy.get('input#password').type('pAssw0rd', { log: false });
       cy.get('select#group').select('driver');
-      cy.get('input#photo').attachFile('images/photo.jpg');
-      cy.get('button').contains('Sign up').click();
-      cy.hash().should('eq', '#/log-in');
-    });
 
-    it('Can log out.', function () {
-      logIn();
-      cy.get('[data-cy="logOut"]').click().should(() => {
-        expect(window.localStorage.getItem('taxi.auth')).to.be.null;
-      });
-      cy.get('[data-cy="logOut"]').should('not.exist');
+      // Handle file upload
+      cy.get('input#photo').attachFile('images/photo.jpg');
+
+      cy.get('button').contains('Sign up').click();
+      cy.wait('@signUp'); // new
+      cy.hash().should('eq', '#/log-in');
     });
     
 
@@ -89,5 +98,31 @@ describe('Authentication', function () {
       );
       cy.hash().should('eq', '#/log-in');
     });
-  
+
+    it('Show invalid fields on sign up error.', function () {
+      cy.intercept('POST', 'sign_up', {
+        statusCode: 400,
+        body: {
+          username: [
+            'A user with that username already exists.'
+          ]
+        }
+      }).as('signUp');
+      cy.visit('/#/sign-up');
+      cy.get('input#username').type('gary.cole@example.com');
+      cy.get('input#firstName').type('Gary');
+      cy.get('input#lastName').type('Cole');
+      cy.get('input#password').type('pAssw0rd', { log: false });
+      cy.get('select#group').select('driver');
+    
+      // Handle file upload
+      cy.get('input#photo').attachFile('images/photo.jpg');
+      cy.get('button').contains('Sign up').click();
+      cy.wait('@signUp');
+      cy.get('div.invalid-feedback').contains(
+        'A user with that username already exists'
+      );
+      cy.hash().should('eq', '#/sign-up');
+    });
+    
 });
